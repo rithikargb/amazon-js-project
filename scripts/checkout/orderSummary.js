8f0353d5322b9f358066baa457739605e1e22310
rithikargb <2205835@kiit.ac.in>
2025-06-08 20:02:44 +0530

    make paymentSummary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
 
@@ -110,6 +111,8 @@ export function renderOrderSummary() {
 
                 const container =  document.querySelector(`.js-cart-item-container-${productId}`);
                 container.remove();
+
+                renderPaymentSummary();
             });
         });
 
@@ -119,6 +122,7 @@ export function renderOrderSummary() {
                 const {productId, deliveryOptionId} = element.dataset;
                 updateDeliveryOption(productId, deliveryOptionId);
                 renderOrderSummary();
+                renderPaymentSummary();
             });
         });
 }
