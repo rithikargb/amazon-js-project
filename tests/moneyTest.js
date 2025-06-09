6dffc3267a707a12356fa383492643bf46a4e722
rithikargb <2205835@kiit.ac.in>
2025-06-09 13:02:36 +0530

    Testing for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,20 @@
+//automated testing
+
+import {formatCurrency} from '../scripts/utils/money';
+
+console.log('test suite: formatCurrency'); 
+
+console.log('converts cents into $'); //group related test cases together
+
+if (formatCurrency(2095) === '20.95') console.log('passed'); //basic test case
+else console.log('failed');
+
+console.log('rounds up to the nearest cent');
+
+if (formatCurrency(2000.5) === '20.01') console.log('passed'); //edge
+else console.log('failed');
+
+console.log('works with 0');
+
+if (formatCurrency(0) === '0.00') console.log('passed'); //edge
+else console.log('failed');
