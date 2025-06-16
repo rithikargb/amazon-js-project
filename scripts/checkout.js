c0463e66a1d5b8d5dadedfe1f1a7f358e53e7db9
rithikargb <2205835@kiit.ac.in>
2025-06-17 00:54:33 +0530

    Learn Promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,37 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 // import '../data/cart-class.js';
-import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
+
+/* Promises practice
+
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value to pass on');
+        });
+    }), 
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+]).then((values) => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+new Promise((resolve) => {
+    console.log('runs this fn. immediately = start promise')
+    /* async code(() => {
+        console.log('finished loading');
+        resolve();
+        })
+    */ /*
+}).then(() => {   //adding a next step for promise
+    console.log('next step')
+})   
+*/ 
 
 renderOrderSummary();
 renderPaymentSummary();
