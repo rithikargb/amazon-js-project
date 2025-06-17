b0e4dca769e92286b2a347d31b372520015553bb
Rithika <2205835@kiit.ac.in>
2025-06-17 20:19:42 +0530

    code cleanup

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,9 +1,3 @@
-/*
-automated testing
-testing framework : ext library that helps write tests easier
-    create test suite/ create tests/ display results
-*/
-
 import {formatCurrency} from '../../scripts/utils/money';
 
 console.log('test suite: formatCurrency'); 
